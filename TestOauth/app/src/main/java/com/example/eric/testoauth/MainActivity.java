83b774ea270134f1ab51bf50dff44c4041835f91
Eric <eric.carvalho@serpro.gov.br>
2016-08-09 21:53:32 -0300

    sending files

---
+++ TestOauth/app/src/main/java/com/example/eric/testoauth/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.eric.testoauth;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity{
+
+    private static final int RC_SIGN_IN = 9001;
+
+
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        SignInButton mGoogleSignInButton = (SignInButton)findViewById(R.id.google_sign_in_button);
+        mGoogleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInWithGoogle();
+            }
+        });
+
+
+    }
+
+    private void signInWithGoogle() {
+        if(mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        final Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if(result.isSuccess()) {
+                final GoogleApiClient client = mGoogleApiClient;
+                result.getSignInAccount().getEmail();
+
+                Toast.makeText(this, "Seja bem-vindo: " + result.getSignInAccount().getDisplayName() +"\n"+result.getSignInAccount().getEmail(), Toast.LENGTH_LONG).show();
+                
+
+
+
+
+
+
+            } else {
+                Log.i("GOOGLE", "Login Fail");
+            }
+        } else {
+            // Handle other values for requestCode
+        }
+
+
+    }
+
+}
